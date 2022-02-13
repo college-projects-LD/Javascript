0264ee4777e44d02b5047f90ade59e6f6f201653
college-projects-LD <delaneyboys16@gmail.com>
2022-02-12 22:06:10 -0700

    n

--- sandbox/w06/week6.js
+++ sandbox/w06/week6.js
@@ -2,30 +2,40 @@
 //fetch(url).then(convertToJason);
 const search_term = document.getElementById('search');
 const sButton = document.getElementById('searchButton');
-const randomButton = document.getElementById('random')
+
 
 const getPokemonData = async query => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${query}`
-    const response = await fetch(url)
+    const url = `https://pokeapi.co/api/v2/pokemon/${query}`;
+    const response = await fetch(url);
 
     if(response.status == 404){
-        document.getElementById('errorCheck').textContent('Error 404 Not Found')
-        return
+        document.getElementById('errorCheck').textContent('Error 404 Not Found');
+        return;
     }
 
-    const pokemon = await response.json()
+    const pokemon = await response.json();
 
-    console.log(pokemon)
+    console.log(pokemon);
 
-    document.getElementById('sprite').setAttribute('src', pokemon.sprites.versions.generation-iv.platinum.front_default)
-    document.getElementById('pokeName').innerHTML = pokemon.name
-    document.getElementById('pokeAbillities').innerHTML = pokemon.abilities.ability.name
-    document.getElementById('HP').innerHTML = `HP ${pokemon.stats[0].base_stats+1}`
-    document.getElementById('pokeType').innerHTML = `${pokemon.types[0].type.name} / ${pokemon.types[1].type.name}
-    `
+    document.getElementById('updateImg').setAttribute('src', pokemon.sprites.versions["generation-iv"].platinum.front_default);
+    document.getElementById('pokeName').innerHTML = pokemon.name;
+    document.getElementById('HP').innerHTML = `HP ${pokemon.stats[0].base_stat}`;
+    const typeContainer = document.getElementById('pokeType');
+  
+    const getPokemonTypes = () => {
+    if (pokemon.types.length === 1){
+        return pokemon.types[0].type.name
+    } else {
+        return `${pokemon.types[0].type.name} / ${pokemon.types[1].type.name}`
+    }
+    }
+
+typeContainer.innerHTML = getPokemonTypes();
+    //document.getElementById('pokeType').innerHTML = `${pokemon.types[0].type.name} / ${pokemon.types[1].type.name}`;
 }
 
-addEventListener('click', () => getPokemonData(search_term.value))
+addEventListener('click', () => getPokemonData(search_term.value));
+
 
 
 
